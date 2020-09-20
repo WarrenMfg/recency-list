982f5b943711e5709e6da3c2678d90b6d471b715
WarrenMfg <warrenmfg@icloud.com>
2020-09-19 22:42:43 -0400

    adds recently viewed aside

--- js/index.ts
+++ js/index.ts
@@ -1,6 +1,30 @@
-const lru = new LRUCache(10);
-const posts = document.getElementById('posts') as HTMLElement;
+// ⚪️ Initialize LRU for posts and recently viewed
+const lruPosts = new LRUCache(10); // keeps posts in view order
+const lruRecentlyViewed = new LRUCache(5); // shows five most recent
 
+// ⚪️ Query DOM
+const posts = document.getElementById('posts') as HTMLElement;
+const modal = document.getElementById('modal') as HTMLElement;
+const recentlyViewed = document.getElementById(
+  'recently-viewed'
+) as HTMLElement;
+
+// ⚪️ Event listeners
+posts.addEventListener('click', e => {
+  const target = e.target as HTMLElement;
+  if (target.tagName === 'BUTTON') {
+    // update modal
+    updateModal(target);
+  }
+});
+
+recentlyViewed.addEventListener('click', e => {
+  const target = e.target as HTMLElement;
+  // update modal
+  updateModal(target);
+});
+
+// ⚪️ Fetch data
 interface Post {
   id: number;
   title: string;
@@ -11,27 +35,16 @@ fetch('../data.json')
   .then((res: { json: () => any }) => res.json())
   .then((d: Post[]) => {
     for (let i = 0; i < d.length; i++) {
-      lru.set(d[i].id, d[i]);
+      lruPosts.set(d[i].id, d[i]);
     }
-    posts.addEventListener('click', e => {
-      const target = e.target as HTMLElement;
-      if (target.tagName === 'BUTTON') {
-        // get the post
-        const post = lru.get(target.dataset.id as string);
-        // get modal
-        const modalTitle = document.getElementById('modalTitle') as HTMLElement;
-        const modalBody = document.getElementById('modalBody') as HTMLElement;
-        // update modal
-        modalTitle.innerText = post.title;
-        modalBody.innerText = post.body;
-      }
-    });
+
     posts.innerHTML = renderDataToDOM();
   })
   .catch(console.log);
 
+// ⚪️ Render to DOM
 function renderDataToDOM(): string {
-  let dll = lru.getDLL();
+  let dll = lruPosts.getDLL();
   let html = '';
   while (dll) {
     html += makeHTML(dll.value);
@@ -42,7 +55,7 @@ function renderDataToDOM(): string {
 
 function makeHTML(node: LRUCacheItem): string {
   return `
-    <div class="card w-50 ml-auto mr-auto mb-3">
+    <div class="card ml-auto mr-auto mb-3">
       <img src="https://placeimg.com/500/250/nature?q=${node.value.id}" class="card-img-top img-fluid" alt="Nature photo" width="500" height="250">
       <div class="card-body d-flex flex-column justify-content-center">
         <h5 class="card-title text-center">${node.value.title}</h5>
@@ -50,4 +63,15 @@ function makeHTML(node: LRUCacheItem): string {
       </div>
     </div>
   `;
+}
+
+function updateModal(target: HTMLElement): void {
+  // get post
+  const post = lruPosts.get(target.dataset.id as string);
+  // update modal
+  modal.dataset.id = target.dataset.id;
+  const modalTitle = modal.querySelector('#modalTitle') as HTMLElement;
+  const modalBody = modal.querySelector('#modalBody') as HTMLElement;
+  modalTitle.innerText = post.title;
+  modalBody.innerText = post.body;
 }
--- js/modalReaction.ts
+++ js/modalReaction.ts
@@ -1,23 +1,33 @@
-const modal = document.getElementById('modal') as HTMLElement;
+// ⚪️ Setup and initialize MutationObserver
 const config = {
   attributeFilter: ['style']
 };
 
-const callback = function (
-  mutationsList: MutationRecord[],
-  observer: MutationObserver
-) {
-  // Use traditional 'for loops' for IE 11
+const callback = function (mutationsList: MutationRecord[]) {
   for (let i = 0; i < mutationsList.length; i++) {
-    const node = mutationsList[i].target as HTMLElement;
-    if (node.style.display === 'none') {
+    const mutatedModal = mutationsList[i].target as HTMLElement;
+    if (mutatedModal.style.display === 'none') {
       posts.innerHTML = renderDataToDOM();
+      const title = mutatedModal.querySelector('#modalTitle') as HTMLElement;
+      const titleText = title.innerText;
+      updateRecentlyViewed(mutatedModal.dataset.id as string, titleText);
     }
   }
 };
 
-// Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
 observer.observe(modal, config);
+
+// ⚪️ Update recently viewed
+function updateRecentlyViewed(id: string, value: string): void {
+  lruRecentlyViewed.set(Number(id), value);
+  let dll = lruRecentlyViewed.getDLL();
+  let html = '';
+
+  while (dll) {
+    html += `<h3 class="rounded recently-viewed" data-id=${dll.value.key} data-toggle="modal" data-target="#modal">${dll.value.value}</h3>`;
+    dll = dll.next;
+  }
+
+  recentlyViewed.innerHTML = html;
+}
